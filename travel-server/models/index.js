93caf7ad8a890805d4a02d4ca994288f32ff7f90
Vishwa Jayasanka <vishvajayasanka@gmail.com>
2021-09-11 20:38:35 +0530

    User Model

--- travel-server/models/index.js
+++ travel-server/models/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: {type: String, required: [true, 'cannot be blank'], unique: true},
   password: {type: String, required: [true, 'cannot be blank']},
-  phone: {type: String, required: [true, 'cannot be blank'], maxlength: 12, minlength: 10}
+  phone: {type: String, required: [true, 'cannot be blank'], maxlength: 12}
 });
 
 mongoose.model('User', UserSchema);
