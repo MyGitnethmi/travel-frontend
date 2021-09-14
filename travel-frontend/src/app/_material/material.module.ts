a1a4b5f509bd737cf26d7a5d034d81f745dcbc99
Vishwa Jayasanka <vishvajayasanka@gmail.com>
2021-09-14 20:31:36 +0530

    Signup View

--- travel-frontend/src/app/_material/material.module.ts
+++ travel-frontend/src/app/_material/material.module.ts
@@ -4,13 +4,17 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatIconModule} from "@angular/material/icon";
 import {MatDividerModule} from "@angular/material/divider";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
 
 const Material = [
   MatInputModule,
   MatButtonModule,
   MatCheckboxModule,
   MatIconModule,
-  MatDividerModule
+  MatDividerModule,
+  MatSelectModule,
+  MatOptionModule
 ];
 
 @NgModule({
--- travel-frontend/src/app/app-routing.module.ts
+++ travel-frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {AuthComponent} from "./auth/auth.component";
 import {LoginComponent} from "./auth/login/login.component";
+import {SignupComponent} from "./auth/signup/signup.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent
+      },
+      {
+        path: 'signup',
+        component: SignupComponent
       },
       {
         path: '',
