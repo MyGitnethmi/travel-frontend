10e74edb6b2384e137ff6270514f817282d912b0
Vishwa Jayasanka <vishvajayasanka@gmail.com>
2021-09-21 18:52:50 +0530

    Search

--- travel-frontend/src/app/home/search/search.component.ts
+++ travel-frontend/src/app/home/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-search',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  search: FormControl
+
+  constructor() {
+    this.search = new FormControl('')
+  }
 
   ngOnInit(): void {
   }
